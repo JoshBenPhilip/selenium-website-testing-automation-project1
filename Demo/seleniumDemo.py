e8dfba6a8db723f14664cd95a5dbd9dd8035a8ce
JoshBenPhilip <joshben4c@gmail.com>
2022-12-14 23:40:45 -0500

    added move up function

--- Demo/seleniumDemo.py
+++ Demo/seleniumDemo.py
@@ -1,9 +1,9 @@
 # Test Case
 # -----------------
-# 1. open web browser (Chrome/firefox/Edge).
+# pause_time. open web browser (Chrome/firefox/Edge).
 # 2. Open URL https://opensource-demo.orangehrmlive.com/
 # 3. Enter username (Admin)
-# 4. Enter Password (admin123)
+# 4. Enter Password (adminpause_time23)
 # 5. Click on Login
 # 6. Capture the title of the homepage. (actual title)
 # 7. Verify title of the page: OrangeHRM (Expected)
@@ -11,10 +11,10 @@
 
 # Test Case
 # -----------------
-# 1. open web browser (Chrome/firefox/Edge).
+# pause_time. open web browser (Chrome/firefox/Edge).
 # 2. Open URL https://opensource-demo.orangehrmlive.com/
 # 3. Enter username (Admin)
-# 4. Enter Password (admin123)
+# 4. Enter Password (adminpause_time23)
 # 5. Click on Login
 # 6. Capture the title of the homepage. (actual title)
 # 7. Verify title of the page: OrangeHRM (Expected)
@@ -25,6 +25,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+pause_time = 2
 
 driver=webdriver.Chrome("/Users/joshbenphilip/Drivers/chromedriver")
 
@@ -32,26 +33,27 @@ driver=webdriver.Chrome("/Users/joshbenphilip/Drivers/chromedriver")
 driver.get("https://www.saucedemo.com/")
 
 driver.maximize_window() # For maximizing window
-driver.implicitly_wait(2) # gives an implicit wait for 20 seconds
+driver.implicitly_wait(1.5) # gives an implicit wait for 20 seconds
 
 driver.find_element(By.ID,"user-name").send_keys("standard_user")
 driver.find_element(By.ID,"password").send_keys("secret_sauce")
 driver.find_element(By.ID, "login-button").click()
-time.sleep(1)
+time.sleep(pause_time)
 driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
-time.sleep(1)
+time.sleep(pause_time)
 driver.find_element(By.ID, "shopping_cart_container").click()
-time.sleep(1)
+time.sleep(pause_time)
 driver.find_element(By.ID,"checkout").click()
-time.sleep(1)
+time.sleep(pause_time)
 driver.find_element(By.ID,"first-name").send_keys("TomTest")
 driver.find_element(By.ID,"last-name").send_keys("Testerson")
 driver.find_element(By.ID,"postal-code").send_keys("33427")
+time.sleep(pause_time)
 driver.find_element(By.ID,"continue").click()
-time.sleep(1)
+time.sleep(pause_time)
+driver.find_element(By.ID, "finish").send_keys("down")
+time.sleep(pause_time)
 driver.find_element(By.ID, "finish").click()
-
-
 #
 exp_result=driver.find_element(By.ID,"checkout_complete_container")
 # image_shown=driver.find_element(By.ID,"checkout_complete_container").img.src="/static/media/pony-express.46394a5d.png"
@@ -63,6 +65,7 @@ if exp_result:
     print ("Test Success")
 else:
     print ("Test Failure")
-time.sleep(1)
+
+time.sleep(pause_time)
 driver.close()
 
